fde10130bd9b234580cfbbf326091300c6813b75
RouSage <therousage@gmail.com>
2019-02-12 19:20:48 +0300

    Some pytorch tests

--- test.py
+++ test.py
@@ -1,6 +1,71 @@
+import numpy as np
 from __future__ import print_function
 import torch
 
-#%%
+# %%
 x = torch.tensor([5.5, 3])
 print(x)
+
+# %%
+x = x.new_ones(5, 3, dtype=torch.double)
+print(x)
+
+# %%
+x = torch.rand_like(x, dtype=torch.float)
+print(x)
+
+# %%
+print(x.size())
+
+# %%
+y = torch.rand(5, 3)
+print(x + y)
+
+# %%
+print(torch.add(x, y))
+
+# %%
+result = torch.empty(5, 3)
+torch.add(x, y, out=result)
+print(result)
+
+# %%
+y.add_(x)
+print(y)
+
+# %%
+print(x[:, 1])
+
+# %%
+x = torch.randn(4, 4)
+y = x.view(16)
+z = x.view(-1, 8)
+print(x.size(), y.size(), z.size())
+
+# %%
+x = torch.randn(1)
+print(x)
+print(x.item())
+
+# %%
+a = torch.ones(5)
+print(a)
+
+# %%
+b = a.numpy()
+print(b)
+
+# %%
+a.add_(1)
+print(a)
+print(b)
+
+# %%
+a = np.ones(5)
+b = torch.from_numpy(a)
+np.add(a, 1, out=a)
+print(a)
+print(b)
+
+# %%
+torch.cuda.is_available()
